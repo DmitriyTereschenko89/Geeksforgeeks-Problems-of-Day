b40dc0af30582350297eec636d30a847610da0ce
Дмитрий Терещенко <dmitriy.tereshchenko1989@gmail.com>
2024-03-25 12:17:02 +0300

    add tasks

---
+++ Print N-bit binary numbers having more 1s than 0s/Program.cs
@@ -0,0 +1,11 @@
+﻿using Print_N_bit_binary_numbers_having_more_1s_than_0s;
+
+int tests = int.Parse(Console.ReadLine());
+Solution solution = new();
+List<string> ans = [];
+for (int t = 0; t < tests; t++)
+{
+	int n = int.Parse(Console.ReadLine());
+	ans.Add(string.Join(",", solution.NBitBinary(n)));
+}
+Console.WriteLine(string.Join("\n", ans));
---
+++ Print N-bit binary numbers having more 1s than 0s/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace Print_N_bit_binary_numbers_having_more_1s_than_0s
+{
+	public class Solution
+	{
+		private void DFS(int n, int zeroCount, int oneCount, List<string> ans, List<char> curSeq)
+		{
+			if (zeroCount > oneCount)
+			{
+				return;
+			}
+			if (curSeq.Count == n)
+			{
+				ans.Add(new string(curSeq.ToArray()));
+				return;
+			}
+			curSeq.Add('0');
+			DFS(n, zeroCount + 1, oneCount, ans, curSeq);
+			curSeq.RemoveAt(curSeq.Count - 1);
+			curSeq.Add('1');
+			DFS(n, zeroCount, oneCount + 1, ans, curSeq);
+			curSeq.RemoveAt(curSeq.Count - 1);
+		}
+		public List<string> NBitBinary(int n)
+		{
+			List<string> ans = [];
+			DFS(n, 0, 0, ans, []);
+			ans.Sort((a, b) => b.CompareTo(a));
+			return ans;
+		}
+	}
+}
