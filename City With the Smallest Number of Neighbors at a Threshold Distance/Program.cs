f0138a1f3a2b864b07f538ac179c7821cfb8e342
Дмитрий Терещенко <dmitriy.tereshchenko1989@gmail.com>
2024-03-27 23:44:51 +0300

    add tasks

---
+++ City With the Smallest Number of Neighbors at a Threshold Distance/Program.cs
@@ -0,0 +1,12 @@
+﻿using City_With_the_Smallest_Number_of_Neighbors_at_a_Threshold_Distance;
+
+int n = 5;
+int m = 5;
+List<List<int>> edges = [[0, 1, 2],
+		 [0, 4, 8],
+		 [1, 2, 3],
+		 [1, 4, 2],
+		 [2, 3, 1],
+		 [3, 4, 1]];
+Solution solution = new();
+Console.WriteLine(solution.FindCity(n, m, edges, 4));
---
+++ City With the Smallest Number of Neighbors at a Threshold Distance/Solution.cs
@@ -0,0 +1,62 @@
+﻿using System.Runtime.InteropServices;
+
+namespace City_With_the_Smallest_Number_of_Neighbors_at_a_Threshold_Distance
+{
+	public class Solution
+	{
+		public int FindCity(int n, int m, List<List<int>> edges, int distanceThreshold)
+		{
+			int[][] adj = new int[n][];
+			for (int i = 0; i < n; ++i)
+			{
+				adj[i] = new int[n];
+				Array.Fill(adj[i], int.MaxValue);
+			}
+			for (int i = 0; i < m; ++i)
+			{
+				int source = edges[i][0];
+				int destination = edges[i][1];
+				int weight = edges[i][2];
+				adj[source][destination] = weight;
+				adj[destination][source] = weight;
+			}
+			for (int i = 0; i < n; ++i)
+			{
+				adj[i][i] = 0;
+			}
+			for (int k = 0; k < n; ++k)
+			{
+				for (int i = 0; i < n; ++i)
+				{
+					for (int j = 0; j < n; ++j)
+					{
+						if (adj[i][k] == int.MaxValue || adj[k][j] == int.MaxValue)
+						{
+							continue;
+						}
+						adj[i][j] = Math.Min(adj[i][j], adj[i][k] + adj[k][j]);
+					}
+				}
+			}
+			int minCity = n + 1;
+			int city = -1;
+			for (int i = 0; i < n; ++i)
+			{
+				int curCountCity = 0;
+				for (int j = 0; j < n; ++j)
+				{
+					if (adj[i][j] <= distanceThreshold)
+					{
+						++curCountCity;
+					}
+				}
+				if (curCountCity <= minCity)
+				{
+					minCity = curCountCity;
+					city = i;
+				}
+			}
+			return city;
+		}
+	}
+}
